680b322fbb9a7f6ea1c56f712c210fe9feb8accd
VitaliyStels <38788206+VitaliyStels@users.noreply.github.com>
2023-02-19 15:42:15 +0200

    Updates

--- src/day-widget.js
+++ src/day-widget.js
@@ -1,9 +1,11 @@
+let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 function getTime() {
 	var now = new Date;
-	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 	var day = days[now.getDay()];
 	var currentTime = now.toLocaleTimeString();
-
+	
+	document.getElementById("timeField").innerHTML = ""
 	document.getElementById("dayField").innerHTML = day
 	document.getElementById("timeField").innerHTML = currentTime
 }
@@ -14,5 +16,5 @@ function getTime() {
 	//console.log("switch func")
 	//print("sw col beige")
 //}
-
-setInterval(getTime, 1000);
+getTime()
+setInterval(getTime, 10000);
