df3c6ec9340f19da6411f324d384a9c258e053df
VitaliyStels <38788206+VitaliyStels@users.noreply.github.com>
2023-03-25 19:42:51 +0200

    basic fixes and improvements

--- src/day-widget.js
+++ src/day-widget.js
@@ -4,17 +4,17 @@ function getTime() {
 	var now = new Date;
 	var day = days[now.getDay()];
 	var currentTime = now.toLocaleTimeString();
-	
+
 	document.getElementById("timeField").innerHTML = ""
 	document.getElementById("dayField").innerHTML = day
 	document.getElementById("timeField").innerHTML = currentTime
 }
 
 //function switchColorBeige() {
-	//var dayFieldElement = document.getElementById("dayField")
-	//dayFieldElement.classList.add("switchColorBeige")
-	//console.log("switch func")
-	//print("sw col beige")
+//var dayFieldElement = document.getElementById("dayField")
+//dayFieldElement.classList.add("switchColorBeige")
+//console.log("switch func")
+//print("sw col beige")
 //}
 getTime()
-setInterval(getTime, 10000);
+setInterval(getTime, 1000);
--- src/switch-color.js
+++
@@ -1,7 +0,0 @@
-function switchColorBeige() {
-	var dayFieldElement = document.getElementById("dayField")
-	dayFieldElement.classList.add("switchColorBeige")
-	console.log("switch func")
-	print("sw col beige")
-}
-
